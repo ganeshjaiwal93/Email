c92f141964f9ae25dd1df457c47aa96ae466c2cc
shantanu7p <shantanu.patwardhan@vshsolutions.com>
2015-09-17 16:41:33 +0530

    added test cases for email validate function

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -1,56 +1,106 @@
 <?php
-namespace emailApp;
-include dirname(__FILE__)."/../vendor/autoload.php";
+
+namespace EmailApp;
 
 class EmailTest extends \PHPUnit_Framework_TestCase {
-	
 	/**
+	 *This test validates email-id according to Gmail standards.
 	 *@test
 	 */
-	public function it_can_validate_email() {
-		$object = new Email();
-		$result = $object->validateEmail("shantanu3687@gmail.com");
-		$this->assertEquals($result, 1);
-	}
-    /**
-	 *@test
-	 *
-	 */
-	public function it_can_validate_email_starting_with_number() {
-		$object = new Email();
-		$result = $object->validateEmail("111shantanu3687@gmail.com");
-		$this->assertEquals($result, 1);
+	public function it_can_validate_emailId_address_according_to_gmail_standards_sample_one() {
+		$email  = new Email();
+		$result = $email->validate("shantanu3687@gmail.com");
+		$this->assertEquals($result, true);
 	}
 	/**
+	 *This test validates email-id according to Gmail standards.
 	 *@test
-	 * @expectedException InvalidArgumentException
 	 */
-	public function it_should_throgh_InvalidArgumentException_for_email_having_more_number_of_at_the_rate_sign() {
-		$object = new Email();
-		$object->validateEmail("shantanu...@3687@gmail.com");
+	public function it_can_validate_emailId_address_according_to_gmail_standards_sample_two() {
+		$emailId = new Email();
+		$result  = $emailId->validate("shantanu3687patwardhan@yahoo.co.in");
+		$this->assertEquals($result, true);
 	}
 	/**
+	 *This function tests Email function Whether it throws InvalidArgumentException or not
+	 *for Emaild starting with number 
 	 *@test
-	 * @expectedException InvalidArgumentException
+	 *@expectedException InvalidArgumentException
 	 */
-	public function it_should_throgh_InvalidArgumentException_for_email_starting_with_special_character() {
-		$object = new Email();
-		$object->validateEmail("$###shantanu3687@gmail.com");
+	public function it_should_throw_InvalidArgumentException_for_emailId_starting_with_numbers() {
+		$email  = new Email();
+		$result = $email->validate("111shantanu3687@gmail.com");
 	}
-    /**
+	/**
+	 *This function tests Email function Whether it throws InvalidArgumentException or not
+	 *for Emaild containing more number of at the rate signs.
 	 *@test
-	 * @expectedException InvalidArgumentException
+	 *@expectedException InvalidArgumentException
 	 */
-	public function it_should_throgh_InvalidArgumentException_for_email_which_does_not_having_at_the_rate_sign() {
-		$object = new Email();
-		$object->validateEmail("shantanu3687gmail.com");
+	public function it_should_throw_InvalidArgumentException_for_emailId_containing_more_number_of_at_the_rate_signs() {
+		$email = new Email();
+		$email->validate("shantanu@@@3687@gmail.com");
 	}
-    /**
+	/**
+	 *This function tests Email function Whether it throws InvalidArgumentException or not
+	 *for Emaild starting with special character.
 	 *@test
-	 * @expectedException InvalidArgumentException
+	 *@expectedException InvalidArgumentException
 	 */
-	public function it_should_throgh_InvalidArgumentException_for_email_which_does_not_having_domain_name() {
-		$object = new Email();
-		$object->validateEmail("shantanu3687@com");
+	public function it_should_throw_InvalidArgumentException_for_emailId_starting_with_special_character() {
+		$email = new Email();
+		$email->validate("$###shantanu3687@gmail.com");
+	}
+	/**
+	 *This function tests Email function Whether it throws InvalidArgumentException or not
+	 *for Emaild which does not contain at the rate sign.
+	 *@test
+	 *@expectedException InvalidArgumentException
+	 */
+	public function it_should_throw_InvalidArgumentException_for_emailId_which_does_not_contain_at_the_rate_sign() {
+		$email = new Email();
+		$email->validate("shantanu3687gmail.com");
+	}
+	/**
+	 *This function tests Email function Whether it throws InvalidArgumentException or not
+	 *for Emaild which does not domain name.
+	 *@test
+	 *@expectedException InvalidArgumentException
+	 */
+	public function it_should_throw_InvalidArgumentException_for_emailId_which_does_not_contain_domain_name() {
+		$email = new Email();
+		$email->validate("shantanu3687@com");
+	}
+
+	/**
+	 *This function tests Email function Whether it throws InvalidArgumentException or not
+	 *for Emaild which contain empty string.
+	 *@test
+	 *@expectedException InvalidArgumentException
+	 */
+	public function it_should_throw_InvalidArgumentException_for_emailId_which_contain_empty_String() {
+		$email = new Email();
+		$email->validate("");
+	}
+	/**
+	 *This function tests Email function Whether it throws InvalidArgumentException or not
+	 *for Emaild which does not contain string.
+	 *@test
+	 *@expectedException InvalidArgumentException
+	 */
+	public function it_should_throw_InvalidArgumentException_for_emailId_which_does_not_contain_string() {
+		$email = new Email();
+		$email->validate(12345789);
+	}
+	/**
+	 *This function tests Email function Whether it throws InvalidArgumentException or not
+	 *for Emaild which contain last character of username as period.
+	 *According to GMail - last character of username should be letter(a-z) or number.
+	 *@test
+	 *@expectedException InvalidArgumentException
+	 */
+	public function it_should_throw_InvalidArgumentException_for_emailId_which_contain_last_character_of_username_as_period() {
+		$email = new Email();
+		$email->validate("shantanu3687.@gmail.com");
 	}
 }
